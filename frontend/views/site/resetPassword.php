826a359070f9f86f317778c6ef97aad6f95dfffe
swaty007 <swaty0007@gmail.com>
2020-04-05 14:20:31 +0300

    v1.11 resset password view

--- frontend/views/site/resetPassword.php
+++ frontend/views/site/resetPassword.php
@@ -6,25 +6,38 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\helpers\Url;
 
 $this->title = 'Reset password';
 ?>
-<div class="site-reset-password">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please choose your new password:</p>
+<div class="access--black">
+    <div class="container">
+        <!--        <div class="row">-->
+        <!--            <div class="col-lg-5">-->
+        <div class="access__content">
+            <h1 class="access__logo">
+                <a href="<?=Url::toRoute(['site/index']);?>"></a>
+                <!--                --><?//= Html::img('@web/img/logo-on-purple.svg', ['class' => '']); ?>
+                <?= Html::img('@web/img/logo-white.svg', ['class' => '']); ?>
+            </h1>
+            <h2 class="access__desc text text--white">
+                <?= $this->title;?>
+            </h2>
+            <?php $form = ActiveForm::begin(['id' => 'reset-password-form', 'options' => ['class' => 'access__form']]); ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
+            <?= $form->field($model, 'password')->label(false)->passwordInput(['autofocus' => true, 'class' => 'form-control access__phone no-border--top']); ?>
 
-                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
-                </div>
+            <div class="form-group">
+                <?= Html::submitButton('Save', ['class' => 'main-btn main-btn--blue main-btn--no-shadow w100 no-border--bottom']) ?>
+            </div>
 
             <?php ActiveForm::end(); ?>
+            <div class="access__footer flex-center">
+                <?= Html::a('I think I remember my password now!', ['site/login'],['class' => 'text text--small text--white']) ?>
+            </div>
         </div>
+        <!--            </div>-->
+        <!--        </div>-->
     </div>
 </div>
